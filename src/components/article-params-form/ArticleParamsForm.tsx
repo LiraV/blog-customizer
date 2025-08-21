ca5bed2328481386c5c58d3622ee1de85fcdd9b5
Valeriia Kharitonova <lerakharitonova2002@gmail.com>
2025-08-21 17:43:13 +0530

    Sprint 10 Iteration 2

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,90 +1,116 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from 'react';
 import styles from './ArticleParamsForm.module.scss';
 import { Select } from 'src/ui/select';
-import { backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions, type ArticleStateType, type OptionType, } from 'src/constants/articleProps';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	type ArticleStateType,
+	type OptionType,
+} from 'src/constants/articleProps';
 import { RadioGroup } from 'src/ui/radio-group';
 import { Text } from 'src/ui/text';
 import { Separator } from 'src/ui/separator';
-
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import clsx from 'clsx';
 
 type Props = {
-	open: boolean;
 	initial: ArticleStateType;
 	onApply: (next: ArticleStateType) => void;
-	onReset: () => void;
-	onClose: () => void;
-}
+};
 
-export const ArticleParamsForm = ({ open, initial, onApply, onReset, onClose }: Props) => {
-
+export const ArticleParamsForm = ({ initial, onApply }: Props) => {
 	const [draft, setDraft] = useState<ArticleStateType>(initial);
 
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const toggle = () => setIsMenuOpen((prev) => !prev);
+
+	const rootRef = useRef<HTMLDivElement>(null);
+
+	useOutsideClickClose({
+		isOpen: isMenuOpen,
+		rootRef,
+		onChange: setIsMenuOpen,
+	});
+
 	useEffect(() => {
-    if (open) setDraft(initial);
-  }, [open, initial]);
+		if (isMenuOpen) setDraft(initial);
+	}, [isMenuOpen, initial]);
 
-  	const update = <K extends keyof ArticleStateType>(key: K, value: ArticleStateType[K]) =>
-    setDraft(prev => ({ ...prev, [key]: value }));
+	const update = <K extends keyof ArticleStateType>(
+		key: K,
+		value: ArticleStateType[K]
+	) => setDraft((prev) => ({ ...prev, [key]: value }));
 
-	const handleSubmit = (e: React.FormEvent) => { 
-		e.preventDefault(); 
-		onApply(draft); 
-		onClose(); 
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onApply(draft);
+		setIsMenuOpen(false);
 	};
 
-  	const handleReset  = (e: React.FormEvent) => { 
-		e.preventDefault(); 
+	const handleReset = (e: React.FormEvent) => {
+		e.preventDefault();
 		setDraft(initial);
-		onReset(); 
+		onApply(initial);
 	};
 
 	return (
 		<>
-			<ArrowButton isOpen={open} onClick={onClose} />
-			<aside className={`${styles.container} ${open ? styles.container_open : ""}`}>
-				<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
-					<Text 
-					as="h2"
-					size={31}
-					weight={800}
-					uppercase
-					align="center">Задайте параметры</Text>
+			<ArrowButton isOpen={isMenuOpen} onClick={toggle} />
+			<aside
+				ref={rootRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isMenuOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text as='h2' size={31} weight={800} uppercase align='center'>
+						Задайте параметры
+					</Text>
 					<div className={styles.spacing}></div>
 					<Select
-      					options={fontFamilyOptions}
-  						selected={draft.fontFamilyOption}
-  						onChange={(opt: OptionType) => update("fontFamilyOption", opt )}
-						title="Шрифт"/>
+						options={fontFamilyOptions}
+						selected={draft.fontFamilyOption}
+						onChange={(opt: OptionType) => update('fontFamilyOption', opt)}
+						title='Шрифт'
+					/>
 					<div className={styles.spacing}></div>
-					<RadioGroup 
+					<RadioGroup
 						name={'fontSize'}
 						options={fontSizeOptions}
 						selected={draft.fontSizeOption}
-						onChange={(opt: OptionType) => update("fontSizeOption", opt )}
+						onChange={(opt: OptionType) => update('fontSizeOption', opt)}
 						title='Размер шрифта'
 					/>
 					<div className={styles.spacing}></div>
 					<Select
-      					options={fontColors}
-  						selected={draft.fontColor}
-  						onChange={(opt: OptionType) => update("fontColor", opt )}
-						title="Цвет шрифта"/>
+						options={fontColors}
+						selected={draft.fontColor}
+						onChange={(opt: OptionType) => update('fontColor', opt)}
+						title='Цвет шрифта'
+					/>
 					<div className={styles.spacing}></div>
-					<Separator/>
-					<div className={styles.spacing}></div>
-					<Select
-      					options={backgroundColors}
-  						selected={draft.backgroundColor}
-  						onChange={(opt: OptionType) => update("backgroundColor", opt )}
-						title="Цвет фона"/>
+					<Separator />
 					<div className={styles.spacing}></div>
 					<Select
-      					options={contentWidthArr}
-  						selected={draft.contentWidth}
-  						onChange={(opt: OptionType) => update("contentWidth", opt )}
-						title="Ширина контента"/>
+						options={backgroundColors}
+						selected={draft.backgroundColor}
+						onChange={(opt: OptionType) => update('backgroundColor', opt)}
+						title='Цвет фона'
+					/>
+					<div className={styles.spacing}></div>
+					<Select
+						options={contentWidthArr}
+						selected={draft.contentWidth}
+						onChange={(opt: OptionType) => update('contentWidth', opt)}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode, CSSProperties } from 'react';
 import clsx from 'clsx';
-import { useState } from "react";
+import { useState } from 'react';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { ArticleStateType, defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,10 +16,7 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
-
 const App = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const toggle = () => setIsOpen(prev => !prev);
 	const [applied, setApplied] = useState<ArticleStateType>(defaultArticleState);
 
 	return (
@@ -31,7 +31,10 @@ const App = () => {
 					'--bg-color': applied.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm open={isOpen} initial={applied} onApply={(next) => setApplied(next)} onReset={() => setApplied(defaultArticleState)} onClose={toggle}/>
+			<ArticleParamsForm
+				initial={applied}
+				onApply={(next) => setApplied(next)}
+			/>
 			<Article />
 		</main>
 	);
